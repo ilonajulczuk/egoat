6247bba4b12f021fac7aa54e5b9d59bb313d30e8
Justyna Ilczuk <att@atte.ro>
2014-05-03 10:36:21 +0200

    Strong refactor of client.

--- client.py
+++ client.py
@@ -10,23 +10,38 @@ import logging
 import socket
 
 
-log = logging
+logger = logging
+
+def convert_address(address):
+    if isinstance(address, (unicode, str)):
+        udp_ip, udp_port = address.split(":")
+    elif len(address) == 2:
+        udp_ip, udp_port = address
+    else:
+        raise ValueError("Wrong address for a socket!")
+    udp_port = int(udp_port)
+    return udp_ip, udp_port
 
 
-def start_connection(peer_address, check_sum, filesize):
-    UDP_IP, UDP_PORT = peer_address
-    UDP_PORT = int(UDP_PORT)
+def sock_send(payload, address, sock=None):
+    if sock is None:
+        sock = socket.socket(socket.AF_INET,  # Internet
+                             socket.SOCK_DGRAM)  # UDP
+    udp_ip, udp_port = convert_address(address)
+    sock.sendto(payload, (udp_ip, udp_port))
+
+
+def sock_bind(address):
+    udp_ip, udp_port = convert_address(address)
     sock = socket.socket(socket.AF_INET,  # Internet
                          socket.SOCK_DGRAM)  # UDP
-    sock.sendto(json.dumps([check_sum, filesize]), (UDP_IP, UDP_PORT))
-    return peer_address
+    sock.bind((udp_ip, udp_port))
+    return sock
 
 
 def send_file(sending_address, filename):
     while True:
         try:
-            UDP_IP, UDP_PORT = sending_address
-            UDP_PORT = int(UDP_PORT)
             sock = socket.socket(socket.AF_INET,  # Internet
                                  socket.SOCK_DGRAM)  # UDP
             chunksize = 1024
@@ -37,75 +52,105 @@ def send_file(sending_address, filename):
                 while sent < file_size:
                     sent += chunksize
                     time.sleep(0.001)
-                    sock.sendto(chunk, tuple(sending_address))
+                    sock_send(chunk, sending_address, sock)
             return True
         except:
             import traceback
-            log.exception(traceback.format_exc())
+            logger.exception(traceback.format_exc())
 
 
-def sender(upload, upload_done):
-    for deal_address, peer_address, checksum, filename in iter(upload.get, 'STOP'):
+def accept_download_request(sock, checksum_files):
+    message, addr = sock.recvfrom(1024)
+    deal_address, sending_address, checksum = json.loads(message)
+    filename = checksum_files[checksum]
+    ack_message = (checksum, os.path.getsize(filename))
+    sock_send(json.dumps(ack_message), deal_address)
+    return sending_address, checksum, filename
+
+
+def request_download(waiting_address, download_address, wanted_checksum):
+    waiting_udp_ip, waiting_udp_port = waiting_address
+    waiting_udp_port = int(waiting_udp_port)
+    downloader_address = ('127.0.0.1', 3300 +
+                          random.randint(0, 20) +
+                          int(str(int(wanted_checksum, 16))[:4]))
+    message = [
+        ((waiting_udp_ip,
+          waiting_udp_port)),
+        downloader_address,
+        wanted_checksum]
+
+    checksum_request = json.dumps(message)
+    sock_send(checksum_request, download_address)
+
+    sock = socket.socket(socket.AF_INET,  # Internet
+                         socket.SOCK_DGRAM)  # udp
+    sock.bind((waiting_udp_ip, waiting_udp_port))
+    json_data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
+    sock.close()
+    checksum, file_size = json.loads(json_data)
+    if checksum != wanted_checksum or file_size == 0:
+        return None
+    else:
+        return downloader_address, file_size
+
+def uploader(upload, upload_done):
+    for peer_address, checksum, filename in iter(upload.get, 'STOP'):
         try:
             result = send_file(peer_address, filename)
             upload_done.put((checksum, peer_address, result))
-            log.info("Sending done!")
+            logger.info("Sending done!")
         except:
             import traceback
-            log.info("Sending failure!")
-            log.exception(traceback.format_exc())
+            logger.info("Sending failure!")
+            logger.exception(traceback.format_exc())
+
+
+def download_file(peer_address, checksum, file_size):
+    udp_ip, udp_port = peer_address
+    udp_port = int(udp_port)
+    sock = socket.socket(socket.AF_INET,  # Internet
+                         socket.SOCK_DGRAM)  # UDP
+    sock.bind((udp_ip, udp_port))
+    downloaded = 0
+    chunksize = 1024
+    downloaded_file = ""
+    with open(checksum, 'w') as f:
+        while downloaded < file_size:
+            chunk, _ = sock.recvfrom(chunksize)
+            downloaded_file += chunk
+            downloaded += len(chunk)
+            f.write(chunk)
+            logger.info(float(downloaded) / file_size)
+    sock.close()
+    return downloaded_file
 
 
 def downloader(download, download_done):
     for peer_address, checksum, file_size in iter(download.get, 'STOP'):
         while True:
             try:
-                UDP_IP, UDP_PORT = peer_address
-                UDP_PORT = int(UDP_PORT)
-                sock = socket.socket(socket.AF_INET,  # Internet
-                                     socket.SOCK_DGRAM)  # UDP
-                sock.bind((UDP_IP, UDP_PORT))
-                downloaded = 0
-                chunksize = 1024
-                downloaded_file = ""
-                with open(checksum, 'w') as f:
-                    while downloaded < file_size:
-                        chunk, addr = sock.recvfrom(chunksize)
-                        downloaded_file += chunk
-                        downloaded += len(chunk)
-                        f.write(chunk)
-                        log.info(float(downloaded) / file_size)
-                sock.close()
-                log.info("Downloaded: %s..." % checksum[:6])
-                download_done.put((checksum, peer_address, True))
-                break
-
+                downloaded_file = download_file(peer_address, checksum, file_size)
+                downloaded_checksum = compute_checksum(downloaded_file)
+                download_correct = downloaded_checksum == checksum
+                if download_correct:
+                    download_done.put((checksum, peer_address, True))
+                    break
             except:
                 import traceback
-                log.exception(traceback.format_exc())
+                logger.exception(traceback.format_exc())
                 time.sleep(4)
-                log.info("will restart soon...")
+                logger.info("will restart soon...")
 
 
 def dealer_upload(upload_address, checksum_files, upload):
-    UDP_IP, UDP_PORT = upload_address.split(":")
-    UDP_PORT = int(UDP_PORT)
-    log.info("will wait at: %s" % upload_address)
-    sock = socket.socket(socket.AF_INET,  # Internet
-                         socket.SOCK_DGRAM)  # UDP
-    sock.bind((UDP_IP, UDP_PORT))
-
+    sock = sock_bind(upload_address)
     while True:
-        message, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
-        time.sleep(1)
-        deal_address, sending_address, checksum = json.loads(message)
-        filename = checksum_files[checksum]
-        ack_message = (checksum, os.path.getsize(filename))
-        deal_UDP_IP, deal_UDP_PORT = deal_address
-        deal_UDP_PORT = int(deal_UDP_PORT)
-        sock.sendto(json.dumps(ack_message), (deal_UDP_IP, deal_UDP_PORT))
-        time.sleep(1)
-        upload.put((deal_address, sending_address, checksum, filename))
+        request = accept_download_request(sock, checksum_files)
+        if request:
+            sending_address, checksum, filename = request
+            upload.put((sending_address, checksum, filename))
+    sock.close()
 
 
 def get_uploader_addresses(server_url, checksum):
@@ -115,67 +160,52 @@ def get_uploader_addresses(server_url, checksum):
     return addresses
 
 
+def choose_peer(server_url, wanted_checksum, downloader_address):
+    all_addresses = get_uploader_addresses(server_url, wanted_checksum)
+    if downloader_address in all_addresses:
+        all_addresses.remove(downloader_address)
+    if not all_addresses:
+        time.sleep(2)
+        logger.info("No one has it!")
+        logger.info("Checksum: %s..." % wanted_checksum[:6])
+    if all_addresses:
+        download_address = random.choice(all_addresses)
+    else:
+        download_address = None
+    return download_address
+
+
 def dealer_download(
         server_url,
-        downloader_address,
+        dealer_downloader_address,
         waiting_address,
         wanted_checksums,
         download):
     while wanted_checksums:
         wanted_checksum = wanted_checksums[0]
-        time.sleep(1)
         try:
-            all_addresses = get_uploader_addresses(server_url, wanted_checksum)
-            if downloader_address in all_addresses:
-                all_addresses.remove(downloader_address)
-            if not all_addresses:
-                time.sleep(2)
-                log.info("No one has it!")
-                log.info("Checksum: %s..." % wanted_checksum[:6])
+            download_address = choose_peer(server_url, wanted_checksum, dealer_downloader_address)
+
+            if download_address is None:
                 continue
-            download_address = all_addresses[0]
-            UDP_IP, UDP_PORT = download_address.split(":")
-            UDP_PORT = int(UDP_PORT)
-            sock = socket.socket(socket.AF_INET,  # Internet
-                                 socket.SOCK_DGRAM)  # UDP
+            response =  request_download(waiting_address, download_address, wanted_checksum)
 
-            waiting_UDP_IP, waiting_UDP_PORT = waiting_address
-            log.info("Here waiting: %s" % waiting_UDP_PORT)
-
-            downloader_address = ('127.0.0.1', 3300 +
-                                  random.randint(0, 20) +
-                                  int(str(int(wanted_checksum, 16))[:4]))
-            message = [
-                ((waiting_UDP_IP,
-                  waiting_UDP_PORT)),
-                downloader_address,
-                wanted_checksum]
-            sock.sendto(json.dumps(message), (UDP_IP, UDP_PORT))
-
-            waiting_UDP_PORT = int(waiting_UDP_PORT)
-            sock = socket.socket(socket.AF_INET,  # Internet
-                                 socket.SOCK_DGRAM)  # UDP
-            sock.bind((waiting_UDP_IP, waiting_UDP_PORT))
-            json_data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
-            sock.close()
-            checksum, filesize = json.loads(json_data)
-            if checksum != wanted_checksum:
-                log.info("Whut?? It's a mistake!")
-                continue
-
-            if filesize:
-                download.put((downloader_address, checksum, filesize))
+            if response is not None:
+                downloader_address, file_size = response
+                download.put((downloader_address, wanted_checksum, file_size))
                 wanted_checksums.remove(wanted_checksum)
-            else:
-                log.info("oh, no deal")
         except:
             import traceback
-            log.exception(traceback.format_exc())
+            logger.exception(traceback.format_exc())
             time.sleep(1)
 
 
-class Client:
-    CHECKSUM_STORAGE = '.checksums'
+def compute_checksum(data):
+    return str(sha512(data).hexdigest())
+
+
+class Client(object):
+    CHECKSUM_STORAGE_BASE = '.checksums'
 
     def __init__(self, directory, server_url, address, waiting_port=6666):
         self.directory = directory
@@ -192,23 +222,23 @@ class Client:
                 for filename in (os.listdir(self.directory))
                 if os.path.isfile(whole_path(self.directory, filename))]
 
-    def compute_check_sums(self, filenames):
-        check_sums = {}
+    def compute_checksums(self, filenames):
+        checksums = {}
         for filename in filenames:
             with open(filename, 'r') as f:
                 file_content = f.read()
-                check_sums[str(sha512(file_content).hexdigest())] = filename
+                checksums[compute_checksum(file_content)] = filename
 
-        return check_sums
+        return checksums
 
     def load_state(self):
         try:
-            storage_filename = self.CHECKSUM_STORAGE + \
+            storage_filename = self.CHECKSUM_STORAGE_BASE + \
                 str(sha512(self.directory).hexdigest())
             with open(storage_filename, 'r') as f:
                 check_sums = json.load(f)
         except IOError:
-            check_sums = self.compute_check_sums(self.discover())
+            check_sums = self.compute_checksums(self.discover())
             with open(storage_filename, 'w') as f:
                 json.dump(check_sums, f)
         return check_sums
@@ -219,7 +249,8 @@ class Client:
                               'address': self.address})
 
     def serve(self, wanted_checksums):
-        NUMBER_OF_PROCESSES = 4
+        NUMBER_OF_UPLOADING_PROCESSES = 4
+        NUMBER_OF_DOWNLOADING_PROCESSES = 4
 
         # Create queues
         task_queue_upload = Queue()
@@ -245,14 +276,14 @@ class Client:
                 task_queue_download)).start()
 
         # Start worker processes
-        for i in range(NUMBER_OF_PROCESSES):
+        for i in range(NUMBER_OF_UPLOADING_PROCESSES):
             Process(
-                target=sender,
+                target=uploader,
                 args=(
                     task_queue_upload,
                     done_queue_upload)).start()
 
-        for i in range(NUMBER_OF_PROCESSES):
+        for i in range(NUMBER_OF_DOWNLOADING_PROCESSES):
             Process(
                 target=downloader,
                 args=(
@@ -267,13 +298,8 @@ class Client:
             time.sleep(2)
             self.announce()
 
-        # Tell child processes to stop
-        for i in range(NUMBER_OF_PROCESSES):
-            task_queue_upload.put('STOP')
-            task_queue_download.put('STOP')
 
-
-def main(*args, **kwargs):
+def main():
     parser = argparse.ArgumentParser(description="P2P file sharing client.")
     parser.add_argument("-q", "--quiet", action="store_true")
 
