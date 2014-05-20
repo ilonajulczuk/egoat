dc4f2e7dd86e42d2d23b15f68670e89b52e78bb3
Justyna Ilczuk <att@atte.ro>
2014-05-20 20:41:27 +0200

    Add documentation to uploader, handle socket error.

--- protocol.py
+++ protocol.py
@@ -21,50 +21,68 @@ CHUNKSIZE = 512
 logger = logging
 
 
+def compute_checksum(data):
+    """Compute sha512 hexdigest of binary string.
+
+    :param data: binary string
+    :return: hexdigest string
+    """
+    return str(sha512(data).hexdigest())
+
+
 class Uploader(object):
 
     def __init__(self, outside_ip='127.0.0.1', inside_ip='0.0.0.0', port=6666):
+        """
+
+        :param outside_ip: will be passed to peer to use in p2p communication
+        :param inside_ip: will be used internally to bind sockets
+        :param port: port to listen for uploading requests
+        """
         self.port = port
         self.outside_ip = outside_ip
         self.inside_ip = inside_ip
 
     @staticmethod
     def stream_file(binding_address, filename):
+        """Stream file `filename` on binding_address.
+
+        :param binding_address: address which will be used to bind streaming socket
+        :param filename:
+        :return:
+        """
         sock = create_socket(socktype="tcp")
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         sock.bind(convert_address(binding_address))
         logger.info("starting listening!")
         sock.listen(5)
-        try:
-            comm_sock, _ = sock.accept()
-            sent = 0
-            file_size = os.path.getsize(filename)
-            with open(filename, 'r') as source_file:
-                while sent < file_size:
-                    chunk = source_file.read(CHUNKSIZE)
-                    comm_sock.send(chunk)
-                    sent += len(chunk)
-            sock.shutdown(1)
-            sock.close()
-            return True
-        except:
-            import traceback
-            logger.exception(traceback.format_exc())
+        comm_sock, _ = sock.accept()
+        sent = 0
+        file_size = os.path.getsize(filename)
+        with open(filename, 'r') as source_file:
+            while sent < file_size:
+                chunk = source_file.read(CHUNKSIZE)
+                comm_sock.send(chunk)
+                sent += len(chunk)
+        sock.shutdown(1)
+        sock.close()
+        return True
 
     def accept_download_request(self, checksum_files):
+        """Accept download requests for given checksum_files.
+
+        :param checksum_files: mapping between checksums and names of files
+        :return: streaming_address, accepted_checksum and filename
+        """
         address = (self.inside_ip, self.port)
         sock = sock_bind(address)
         message, _ = sock.recvfrom(1024)
 
         sock.close()
         loaded_json = json.loads(message)
-        try:
-            deal_address, checksum = loaded_json
-            filename = checksum_files[checksum]
-        except:
-            deal_address = loaded_json['waiting_address']
-            checksum = loaded_json['checksum']
-            filename = checksum_files[checksum]
+        deal_address = loaded_json['waiting_address']
+        checksum = loaded_json['checksum']
+        filename = checksum_files[checksum]
 
         binding_port = 7381
 
@@ -117,33 +135,28 @@ class Downloader(object):
         return download_address
 
     def request_download(self, download_address, wanted_checksum):
-        try:
-            # outside address
-            message = {
-                "waiting_address": ":".join((self.outside_ip,
-                str(self.downloader_port))),
-                "checksum": wanted_checksum}
+        message = {
+            "waiting_address": ":".join((self.outside_ip,
+            str(self.downloader_port))),
+            "checksum": wanted_checksum}
 
-            downloader_address = ":".join((self.inside_ip,
-                                          str(self.downloader_port)))
+        downloader_address = ":".join((self.inside_ip,
+                                      str(self.downloader_port)))
 
-            checksum_request = json.dumps(message)
-            sock = sock_bind(downloader_address)
-            sock_send(checksum_request, download_address)
+        checksum_request = json.dumps(message)
+        sock = sock_bind(downloader_address)
+        sock_send(checksum_request, download_address)
 
-            json_data, _ = sock.recvfrom(1024)
-            sock.close()
-            payload = json.loads(json_data)
-            peer_uploader_address = payload['streaming_address']
-            checksum = payload['checksum']
-            file_size = payload['file_size']
-            if checksum != wanted_checksum or file_size == 0:
-                return None
-            else:
-                return peer_uploader_address, file_size
-        except:
-            import traceback
-            traceback.print_exc()
+        json_data, _ = sock.recvfrom(1024)
+        sock.close()
+        payload = json.loads(json_data)
+        peer_uploader_address = payload['streaming_address']
+        checksum = payload['checksum']
+        file_size = payload['file_size']
+        if checksum != wanted_checksum or file_size == 0:
+            return None
+        else:
+            return peer_uploader_address, file_size
 
     def download_file(self, peer_address, checksum, file_size):
         try:
@@ -164,11 +177,7 @@ class Downloader(object):
 
             sock.close()
             return downloaded_file[:file_size]
-        except:
+        except socket.error:
             import traceback
-            logger.warning(traceback.format_exc())
+            logger.exception(traceback.format_exc())
             return ""
-
-
-def compute_checksum(data):
-    return str(sha512(data).hexdigest())
